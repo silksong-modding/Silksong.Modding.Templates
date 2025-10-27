fdd9182bae059eb3f389da23c06c62aef33d8037
Sean Dempsey <dempsey.sean@outlook.com>
2025-10-27 00:52:51 -0700

    Give up on the workaround and change from tmp to temp

--- Silksong.Modding.Templates.Tests/SilksongPluginTest.cs
+++ Silksong.Modding.Templates.Tests/SilksongPluginTest.cs
@@ -26,11 +26,7 @@ public class SilksongPluginTest(ITestOutputHelper output)
                 "packages.lock.json",
                 "thunderstore/icon.png"
             ),
-        }.WithCustomScrubbers(
-            // on Linux, the `thunderstore/tmp` path is erroneously replaced with `thunderstore{TempPath}` with disastrous consequences
-            // https://github.com/silksong-modding/Silksong.Modding.Templates/actions/runs/18831278154/job/53723055893
-            ScrubbersDefinition.Empty.AddScrubber(content => content.Replace("{TempPath}", "/tmp"))
-        );
+        };
 
         VerificationEngine engine = new(logger);
 
