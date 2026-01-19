06de3999a6d759d07d1bd23d619ce77254fe6c36
Sean Dempsey <dempsey.sean@outlook.com>
2026-01-18 17:09:19 -0800

    Enable choosing monodetour as the hook style

---
+++ Silksong.Modding.Templates.Tests/Snapshots/SnapshotTest.silksongplugin.MonoDetour.verified/silksongplugin/SilksongTemplateTesterPlugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace SilksongTemplateTester;
+
+// TODO - adjust the plugin guid as needed
+[BepInAutoPlugin(id: "io.github.myusername.silksongtemplatetester")]
+public partial class SilksongTemplateTesterPlugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Put your initialization logic here
+        Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+    }
+}
